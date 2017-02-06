b9f6554749d95cf4132af478e09aa47dcfb26d33
Karim Hmaissi <karim.hmaissi@LT-21278.local>
2017-02-06 11:48:47 +0000

    Added static data set for spent

--- js/main.js
+++ js/main.js
@@ -5,6 +5,31 @@
 	var amountLeft = BILLION;
 	var clusterize = null;
 
+	var amountOfRows = 10000;
+	var numberOfBoxesInRow = 10;
+
+
+	var dataArray = function () {
+
+		var finishedArray = [];
+
+		for(var i = 0; i < amountOfRows; i++) {
+			var newRow = [];
+
+			for(var x = 0; x < numberOfBoxesInRow; x++) {
+				newRow.push(false);
+			}
+
+			finishedArray.push(newRow);
+		}
+
+		return finishedArray;
+	}();
+
+	console.log(dataArray);
+
+
+
 	var products = [
 		{
 		category: 'privateIslands',
@@ -72,16 +97,65 @@
 	}
 
 
-	var getData = function (number) {
-		var data = [];
-		for(var i = 0; i < number; i++) {
-			var x = i + 1; //0 offset
+	var getData = function () {
+		// var data = [];
+		// for(var i = 0; i < number; i++) {
+		// 	var x = i + 1; //0 offset
 
-			data.push('<li><ul class="billions__inner"><span class="label">$'+ (x * 100000) + '</span><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li></ul></li>')
+		// 	data.push('<li><ul class="billions__inner"><span class="label">$'+ (x * 100000) + '</span><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li></ul></li>')
+		// }
+
+		// return data;
+
+
+		var data = [];
+
+		for(var i = 0; i < dataArray.length; i++) {
+			//loop over rows
+			var currentRow = dataArray[i];
+			// var $newLiForRow = $('<li><ul class="billions__inner"><span class="label">' + number + '</span></ul></li>');
+			var newRowString = ['<li><ul class="billions__inner"><span class="label">' + ((i + 1) * 100000) + '</span>'];
+
+			for(var x = 0; x < currentRow.length; x++) {
+				//loop over blocks in row
+				// $newLiForRow.append(currentRow[x]);
+
+				if(currentRow[x] === false) {
+					//not spent
+					newRowString.push('<li></li>');
+				} else {
+					//spent
+					newRowString.push('<li class="spent"></li>');
+				}
+
+			}
+
+			newRowString.push('</ul></li>');
+
+			var finishedRow = newRowString.join('');
+
+			data.push(finishedRow);
 		}
 
 		return data;
 	}
+
+
+	var updateDataArray = function(amountSpent) {
+		console.log('About to change the data array fam');
+
+		//find current position ?
+		var offset = (BILLION - amountLeft) / 100000;
+
+		//loop over data and update for amount
+		var numberOfRows = amountSpent / 100000;
+
+		for(x = 0; x< numberOfRows + offset; x++) {
+			for(var i = 0; i < dataArray[x].length; i++) {
+				dataArray[x][i] = true;
+			}
+		}
+	};
 
 	var initProducts = function() {
 
@@ -134,6 +208,10 @@
 			e.preventDefault();
 
 			var amount =  $(this).attr('data-price');
+
+			updateDataArray(amount);
+
+			
 			amountLeft -= parseInt(amount, 10);
 
 			// $('#amount-left').text(amountLeft);
@@ -150,7 +228,6 @@
 			  },
 			  complete: function() {
 			    $('#amount-left').text(this.countNum);
-			    // alert('finished');
 			  }
 			});
 
@@ -162,8 +239,24 @@
 
 			var updatedAmount = amountLeft / 100000;
 
-			var newData = getData(updatedAmount);
+
+
+
+			console.log('AmountSpent: ', amount);
+			console.log('AmountLeft: ', amountLeft);
+
+			//animate the boxes going white
+
+			//then update the data array
+			
+			var newData = getData();
+
+			//then republish data to clusrerize
 			clusterize.update(newData);
+
+
+			
+
 			console.log('Iv updated the data fam');
 		});
 	}
@@ -180,17 +273,3 @@
 
 
 
-
-
-
-// $({countNum: 99}).animate({countNum: 1000}, {
-//   duration: 8000,
-//   easing:'linear',
-//   step: function() {
-//     // What todo on every count
-//     console.log(Math.floor(this.countNum));
-//   },
-//   complete: function() {
-//     console.log('finished');
-//   }
-// })
